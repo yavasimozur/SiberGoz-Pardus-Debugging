da3fb38ef1f2fd6b35eb04a156a2cc43e4112fab
yavasimozur <148590119+yavasimozur@users.noreply.github.com>
2026-07-31 21:34:32 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config:
     """Sistem genelinde kullanılacak statik ayarlar (Sınıf Özellikleri)"""
     
-    ESP32_CAMERA_URL = os.getenv("ESP32_CAMERA_URL", "http://10.32.60.123/capture")
+    ESP32_CAMERA_URL = os.getenv("ESP32_CAMERA_URL", "http://192.168.1.Y/capture")
     DATABASE_PATH = os.getenv("DATABASE_PATH", "database/SiberGoz.db")
     PCAP_SAVE_DIR = os.getenv("PCAP_SAVE_DIR", "logs/captures/")
     LOG_FILE_PATH = os.getenv("LOG_FILE_PATH", "logs/system.log")
